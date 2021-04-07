544cdd25141deef9520464d895166a87ee5c309b
ParkJunHee <junhee916@naver.com>
2021-04-07 21:16:28 +0900

    프로젝트생성

---
+++ node_Basic/schemas/boards.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+const boardSchema = new Schema({
+
+    title :{
+        type: String,
+    },
+    contents : {
+        type: String,
+    },
+    nickname : {
+        type: String,
+    }
+})
+
+boardSchema.virtual('boardId').get(function () {
+	return this._id.toHexString();
+});
+
+boardSchema.set('toJSON', {
+	virtuals: true
+});
+
+module.exports = mongoose.model("Boards", boardSchema)
---
+++ node_Basic/schemas/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const {Schema} = mongoose;
+const userSchema = new Schema({
+
+    names :{
+        type: String,
+    },
+    password : {
+        type: String,
+    },
+    nickname : {
+        type: String,
+    }
+})
+
+userSchema.virtual('userId').get(function () {
+	return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+	virtuals: true
+});
+
+
+module.exports = mongoose.model("Users", userSchema)
